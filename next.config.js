e30cb55b77fd242bf728fc181f25d796c1567a7c
nikitays <nikitays0987@gmail.com>
2023-03-26 19:49:13 +0300

    - build front

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   // output: "export",
-  basePath: "/cootelsLandingPage",
+  // basePath: "/cootelsLandingPage",
+  assetPrefix: isProd
+    ? "https://shishlovfelix.github.io/cootelsLandingPage"
+    : undefined,
 };
 
 module.exports = nextConfig;
