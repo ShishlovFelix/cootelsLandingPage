3d1f2e34d6e991423722c20abd1bb185439f09e2
nikitays <nikitays0987@gmail.com>
2023-03-29 22:56:07 +0300

    - build front

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "shishlovfelix.github.io",
+        port: "",
         pathname: "/cootelsLandingPage/**",
       },
     ],
